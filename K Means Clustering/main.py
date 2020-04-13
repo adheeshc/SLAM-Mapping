53638ddc16ca114e1853e9865eb4f781411daac3
Adheesh <adheeshchat@gmail.com>
2020-04-12 22:22:08 -0400

    init commit

---
+++ K Means Clustering/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#   ___      _ _                    _     
+#  / _ \    | | |                  | |    
+# / /_\ \ __| | |__   ___  ___  ___| |__  
+# |  _  |/ _` | '_ \ / _ \/ _ \/ __| '_ \ 
+# | | | | (_| | | | |  __/  __/\__ \ | | |
+# \_| |_/\__,_|_| |_|\___|\___||___/_| |_|
+# Date:   2020-04-12 22:17:00
+# Last Modified time: 2020-04-12 22:21:39
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+
+
+def main():
+	print(__file__+" running...")
+	
